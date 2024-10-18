2b83511aae07ccc36a3d3ff5cf2b23a1149ad1cf
David <dcasado@users.noreply.github.com>
2024-10-18 19:27:46 +0200

    Improve styling and add template for navigation

--- src/templates.rs
+++ src/templates.rs
@@ -3,6 +3,8 @@ use handlebars::TemplateError;
 pub fn register(handlebars: &mut handlebars::Handlebars) -> Result<(), TemplateError> {
     handlebars.register_template_file("base", "./templates/base.hbs")?;
 
+    handlebars.register_template_file("navigation", "./templates/navigation.hbs")?;
+
     handlebars.register_template_file("index", "./templates/index.hbs")?;
 
     handlebars.register_template_file("chart", "./templates/chart.hbs")?;
